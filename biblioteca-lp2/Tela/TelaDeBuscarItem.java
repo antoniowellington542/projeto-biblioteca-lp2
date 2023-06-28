d0a3311e7fe0b8c6f94439cc31288163568f3305
Daniel de Gusmão Moraes <92873057+dangusmao@users.noreply.github.com>
2023-06-28 17:59:57 -0300

    Update TelaDeBuscarItem.java

--- biblioteca-lp2/Tela/TelaDeBuscarItem.java
+++ biblioteca-lp2/Tela/TelaDeBuscarItem.java
@@ -3,4 +3,34 @@ package Tela;
 import javax.swing.*;
 
 public class TelaDeBuscarItem extends JPanel {
+  
+  private Item buscarPorId(String id)
+  {    
+     item = Biblioteca.biblioteca.getItemPorId(id);
+     if(item != null)
+     {
+       JOptionPane.showMessageDialog(null, "Item encontrado", "Sucesso", JOptionPane.WARNING_MESSAGE);
+       return item;
+     }
+       
+    else
+    {
+       JOptionPane.showMessageDialog(null, "O item buscado não encontra-se no estoque", "Alerta", JOptionPane.WARNING_MESSAGE);     
+    }
+  }
+
+  private Item buscarPorNome(String nome)
+  {    
+     item = Biblioteca.biblioteca.getItemPorNome(nome);
+     if(item != null)
+     {
+       JOptionPane.showMessageDialog(null, "Item encontrado", "Sucesso", JOptionPane.WARNING_MESSAGE);
+       return item;
+     }
+       
+    else
+    {
+       JOptionPane.showMessageDialog(null, "O item buscado não encontra-se no estoque", "Alerta", JOptionPane.WARNING_MESSAGE);     
+    }
+  }  
 }
